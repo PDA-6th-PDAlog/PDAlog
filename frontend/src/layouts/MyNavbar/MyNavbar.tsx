b60c31336a635a5bff8181d4a2e907d5f8d49579
김혜윤 <gpdbs9409@naver.com>
2025-06-25 10:37:59 +0900

    navbar생성

--- frontend/src/layouts/MyNavbar/MyNavbar.tsx
+++ frontend/src/layouts/MyNavbar/MyNavbar.tsx
@@ -1,52 +1,57 @@
-// 'use client';
-// import Link from 'next/link';
-// import Router from 'next/navigation';
-// export default function MyNavbar() {
-// 	return (
-// 		<div className="bg-blue-200">
-// 			<ul className="flex justify-center gap-4 items-center">
-// 				<li>
-// 					<Link href="/">Home</Link>
-// 				</li>
-// 				<li>
-// 					<Link href="/portfolio">포트폴리오</Link>
-// 				</li>
-// 				{/* <div><button></button>onClick=(() => {
-//                     Router.push("/portfolio")})포트폴리오
-//                     </div> */}
-// 				<li>
-// 					<Link href="/posts">Posts</Link>
-// 				</li>
-// 			</ul>
-// 		</div>
-// 	);
-// }
-
 "use client";
-// import Link from "next/link";
-// import Router from "next/navigation";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
+import Image from "next/image";
+import Link from "next/link";
 
-function CollapsibleExample() {
+export default function AppNavbar() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">프로젝트</Navbar.Brand>
+        <Navbar.Brand as={Link} href="/">
+          <Image
+            src="/assets/신한투자증권.jpg"
+            alt="로고"
+            width={120}
+            height={40}
+            style={{ objectFit: "contain" }}
+          />
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="posts">Posts</Nav.Link>
-            <Nav.Link href="client-albums">Client-albums</Nav.Link>
-            <Nav.Link href="boards">Boards</Nav.Link>
+          <Nav className="ms-3 d-flex gap-2">
+            <Nav.Link
+              as={Link}
+              href="/login"
+              style={{
+                backgroundColor: "#007bff",
+                color: "white",
+                borderRadius: "8px",
+                padding: "6px 12px",
+              }}
+            >
+              로그인
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              href="/signup"
+              style={{
+                backgroundColor: "#007bff",
+                color: "white",
+                borderRadius: "8px",
+                padding: "6px 12px",
+              }}
+            >
+              회원가입
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
-
-export default CollapsibleExample;
