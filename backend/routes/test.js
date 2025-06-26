a29f21b5ff03292ed616cb06b08e6945215186b5
JeongHyeon Jo <cisxo@naver.com>
2025-06-26 09:31:55 +0900

    initial

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer');
+const fs = require('fs');
 
 const testController = require("../controller/testController");
+const {uploadFile} = require("../service/upladS3Service");
+const {extname} = require("node:path");
 
 /**
  * @swagger
@@ -14,6 +18,24 @@ const testController = require("../controller/testController");
  *       200:
  *         description: 성공
  */
-router.get("/test", testController.getAllTests);
+router.get("/", testController.getAllTests);
+
+const upload = multer({dest: 'uploads/'})
+
+router.post('/upload', upload.single('file'), async (req, res) => {
+    try {
+        const localFilePath = req.file.path;
+        const s3Key = Date.now() + extname(req.file.originalname);
+        const result = await uploadFile(localFilePath, s3Key);
+
+        fs.unlinkSync(localFilePath);
+
+        res.json({ url: result.Location });
+    } catch (err) {
+        console.error('Upload error:', err);
+        res.status(500).json({ error: 'S3 upload failed' });
+    }
+
+});
 
 module.exports = router;
