5313792930d62fa832e8f87e7bbfdffc27ecfbae
김혜윤 <gpdbs9409@naver.com>
2025-06-29 00:35:52 +0900

    증맬돌아가는데

---
+++ backend/common/middlewareAuth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const authenticate = (req, res, next) => {
+  try {
+    let token = req.cookies.authToken;
+    const headerToken = req.headers.authorization;
+
+    if (!token && headerToken?.startsWith("Bearer ")) {
+      token = headerToken.split(" ")[1];
+    }
+
+    if (!token) {
+      return res.status(401).json({ message: "인증 토큰이 없습니다." });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: "유효하지 않은 토큰입니다." });
+  }
+};
+
+module.exports = authenticate;
